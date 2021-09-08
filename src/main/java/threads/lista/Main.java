4ee3efeb033ee9d64f03db1a0c27e10317b2ae45
hshima <shimada.henrique@gmail.com>
2021-09-08 00:25:07 -0300

    Testa List::add usando ArrayList - non threadsafe

--- src/main/java/threads/lista/Main.java
+++ src/main/java/threads/lista/Main.java
@@ -1,18 +1,23 @@
 package threads.lista;
 
-import threads.lista.model.Lista;
+import java.util.ArrayList;
+import java.util.List;
+
 import threads.lista.runner.AddElementRunner;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		Lista list  = new Lista();
+		//Lista list  = new Lista();
+		
+		List<String> list = new ArrayList<>();
 		for(int i =0; i < 100; i++) {
 			new Thread(new AddElementRunner(list, i)).start();;
 		}
 		Thread.sleep(2000);
 		for(int i =0; i < list.size(); i++) {
-			System.out.println(i + " - " + list.getElement(i));
+//			System.out.println(i + " - " + list.getElement(i));
+			System.out.println(i + " - " + list.get(i));
 		}
 	}
 
--- src/main/java/threads/lista/runner/AddElementRunner.java
+++ src/main/java/threads/lista/runner/AddElementRunner.java
@@ -1,16 +1,19 @@
 package threads.lista.runner;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
-import threads.lista.model.Lista;
 
 @AllArgsConstructor
 public class AddElementRunner implements Runnable {
 
-	private Lista list;
+//	private Lista list;
+	private List<String> list;
 	private int n;
 	public void run() {
 		for(int i =0; i < 10; i++) {
-			list.addElements("Thread: "+n+" - " + i);
+//			list.addElements("Thread: "+n+" - " + i);
+			list.add("Thread: "+n+" - " + i);
 		}
 	}
 
